56d784517eba0de5279f646487098997f047553a
Douglas <douglasgia@hotmail.com>
2024-05-10 08:56:10 -0300

    Conserto de estilo

--- Portifolio/src/Form/index.tsx
+++ Portifolio/src/Form/index.tsx
@@ -5,15 +5,28 @@ import "./style.scss";
 const Form = () => {
   return (
     <form action="#" method="post" className="form">
-      <input type="text" placeholder="Seu nome" name="nome" id="nome" className="form-items"/>
-      <input type="email" name="email" placeholder="Seu e-mail" id="email" className="form-items"/>
       <input
-        type="content"
+        type="text"
+        placeholder="Seu nome"
+        name="nome"
+        id="nome"
+        className="form-items"
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Seu e-mail"
+        id="email"
+        className="form-items"
+      />
+      <textarea placeholder="Assunto">teste</textarea>
+      <input
+        type=""
         name="content"
         placeholder="Assunto"
         className="form-items"
       />
-      <input type="submit" className="form-items"/>
+      <input type="submit" className="form-items" />
     </form>
   );
 };
