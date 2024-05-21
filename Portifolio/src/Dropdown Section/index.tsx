bb1d30683948fdf218d1d0494d67766fdde97a25
Douglas <douglasgia@hotmail.com>
2024-05-21 15:18:44 -0300

    Guarda alterações

---
+++ Portifolio/src/Dropdown Section/index.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./style.scss";
+
+interface ListItem {
+  title: string;
+  paragraph: string;
+}
+
+interface IPropsDropdownSection {
+  items: ListItem[];
+}
+
+const DropdownSection: React.FC<IPropsDropdownSection> = ({ items }) => {
+  const [openDropdownIndex, setOpenDropdownIndex] = useState<number | null>(
+    null
+  );
+
+  const toggleDropdown = (index: number) => {
+    setOpenDropdownIndex(index === openDropdownIndex ? null : index);
+  };
+
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>
+          <button onClick={() => toggleDropdown(index)}>{item.title}</button>
+          {openDropdownIndex === index && <p>{item.paragraph}</p>}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default DropdownSection;
---
+++ Portifolio/src/Main/Rectangle/intedx.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Avatar from "../../assets/img/Rectangle.png";
+
+import "./style.scss";
+
+const Rectangle = () => {
+  return (
+    <>
+      <img src={Avatar} alt="Retângulo colorido" className="rectangle"/>
+    </>
+  );
+};
+
+export default Rectangle;
